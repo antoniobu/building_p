7dcd082b8cc79cdadd9cf4b58f5a61d2c58747ee
antoniobu <anton.buhrymenko@nure.ua>
2024-05-30 13:17:38 +0300

    imgfix

--- building_project/apps/displaysite/urls.py
+++ building_project/apps/displaysite/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'displaysite'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:product_id>/', views.product, name='product'),
+    path(r'^$', views.product, name='product'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
